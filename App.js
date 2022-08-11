c2f1395dc15cac62484b240dadfab5447aa4591c
aexomir <rnj.amirhossein1@gmail.com>
2022-08-11 18:37:28 +0430

    initialized action button

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, LogBox} from 'react-native';
+import ActionButton from './src/components/UI/Buttons/ActionButton';
 
 export default App = props => {
-  return <View style={styles.container}></View>;
+  LogBox.ignoreAllLogs(true);
+
+  return (
+    <View style={styles.container}>
+      <ActionButton />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    // padding: 20,
     justifyContent: 'center',
     alignItems: 'center',
   },
--- src/components/UI/Buttons/actionButton.js
+++ src/components/UI/Buttons/actionButton.js
@@ -5,29 +5,82 @@ import React from 'react';
 import {View, StyleSheet, Image} from 'react-native';
 import RNActionButton from 'react-native-action-button';
 
-export default ActionButton = props => {
+import Text from '../../function/Text';
+
+const DUMMY_OPTIONS = [
+  {
+    id: 1,
+    name: 'Facebook',
+    color: '#3b5998',
+    onPress: () => {
+      console.log('Facebook');
+    },
+  },
+  {
+    id: 2,
+    name: 'Google',
+    color: '#dd4b39',
+    onPress: () => {
+      console.log('Google');
+    },
+  },
+  {
+    id: 3,
+    name: 'Github',
+    color: '#24292e',
+    onPress: () => {
+      console.log('Github');
+    },
+  },
+];
+
+export default ActionButton = ({
+  actions = DUMMY_OPTIONS,
+  color = '#24292e',
+  size = 60,
+  degrees = 135,
+  position = 'center',
+  renderMainIcon,
+  onToggle,
+  onReset,
+  ...props
+}) => {
+  const renderAction = ({id, color, title, iconName, onPress}) => {
+    return (
+      <RNActionButton.Item
+        key={id}
+        buttonColor={color}
+        title={title}
+        onPress={onPress}
+        size={id == 2 ? 55 : 45}>
+        {/* <Icon name={iconName} style={styles.actionButtonIcon} /> */}
+        <Image
+          source={{uri: 'https://picsum.photos/200/300'}}
+          style={{
+            width: 200,
+            height: 300,
+            borderWidth: 1,
+          }}
+        />
+        {/* <Text>{title}</Text> */}
+      </RNActionButton.Item>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <RNActionButton buttonColor="rgba(231,76,60,1)">
-        <RNActionButton.Item
-          buttonColor="#9b59b6"
-          title="New Task"
-          onPress={() => console.log('notes tapped!')}>
-          {/* <Icon name="md-create" style={styles.actionButtonIcon} /> */}
-          <Image source={'https://picsum.photos/200'} />
-        </RNActionButton.Item>
-        {/* <RNActionButton.Item
-          buttonColor="#3498db"
-          title="Notifications"
-          onPress={() => {}}>
-          <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-        </RNActionButton.Item>
-        <RNActionButton.Item
-          buttonColor="#1abc9c"
-          title="All Tasks"
-          onPress={() => {}}>
-          <Icon name="md-done-all" style={styles.actionButtonIcon} />
-        </RNActionButton.Item> */}
+      <RNActionButton
+        buttonColor={color}
+        position={position}
+        renderIcon={renderMainIcon}
+        onPress={onToggle}
+        onReset={onReset}
+        size={size}
+        degrees={degrees}
+        {...props}>
+        {actions.map(action => {
+          return renderAction(action);
+        })}
       </RNActionButton>
     </View>
   );
