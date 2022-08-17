3038bc98dd29b36a53c6db685b8a254a3bf9fe93
aexomir <rnj.amirhossein1@gmail.com>
2022-08-17 21:04:50 +0430

    added react-native-vector-icons

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {View, StyleSheet, Text, LogBox, TouchableOpacity} from 'react-native';
 
 import Alert from './src/components/UI/Alert';
-import Drawer from './src/components/UI/Drawer';
+import Icon from './src/components/UI/Icon';
 
 export default App = props => {
   LogBox.ignoreAllLogs(true);
 
   return (
     <View style={styles.container}>
+      <Icon />
       {/* This Alert Component is for initialization; => Don't remove it! */}
       <Alert />
     </View>
--- src/components/UI/icons.js
+++
@@ -1,1 +0,0 @@
-// https://github.com/oblador/react-native-vector-icons
